41181152c49daba1f7b494fe796be32850d67d18
FracturedDiamond <49692399+FracturedDiamond@users.noreply.github.com>
2021-04-02 10:18:23 -0700

    Add files via upload

---
+++ Scripts/Enemy/Nodes/HealthNode.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthNode : Node
+{
+    private EnemyAI ai;
+    private float threshold;
+
+    public HealthNode(EnemyAI ai, float threshold)
+    {
+        this.ai = ai;
+        this.threshold = threshold;
+    }
+
+    public override NodeState Evaluate()
+    {
+        Debug.Log("HealthNode: " + ai._currentHealth + " threshold: " + threshold);
+        return ai._currentHealth <= threshold ? NodeState.SUCCESS : NodeState.FAILURE;
+    }
+
+}
